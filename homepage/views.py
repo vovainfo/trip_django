361e0b156908d0534bcd9a2380b3b89f365a75cb
Vladimir Gumennikov <vova@vova.info>
2021-09-20 01:32:35 +1000

    первая рабочая версия

--- homepage/views.py
+++ homepage/views.py
@@ -1,35 +1,169 @@
 from django.shortcuts import render
+from trip.trip_api import get_route_list
+from trip.trip_api import pretty_datetime
+
+import json
+
 
 def index(request):
-    icecreams = ''
-    friends = ''
-    city_weather = ''
-    friend_output = ''
-    selected_icecream = ''
-    # В переменную conclusion будет сохранен текст рекомендации
-    conclusion = ''
-
-    friends += (f'<input type="radio" name="friend"'
-                f' required value="Жопа">Жопа1<br>')
-
-    ice_form = (f'<input type="radio" name="icecream" required'
-                f' value="Жопа2">Жопа3')
-
-    ice_link = f'<a href="icecream/">Узнать состав</a>'
-    icecreams += f'{ice_form} | {ice_link} <br>'
-
-
-    if request.method == 'POST':
-        friend_output = f'Жопа5, тебе прислали Жопа6!'
-        city_weather = f'В городе Жопа7 погода: Жопа8'
 
     context = {
-        'icecreams': icecreams,
-        'friends': friends,
-        'friend_output': friend_output,
-        'city_weather': city_weather,
-        'conclusion': conclusion,
-        # Передайте значение conclusion в шаблон
-
+        'base_airport_value': f'value=UUEE',
+        'minimum_departure_date_value': f'value=2021-10-01T00:00',
+        'maximum_return_date_value': f'value=2021-10-02T00:00',
+        'minimum_stay_value': f'value=5'
     }
+    if request.method == 'POST':
+        base_airport = request.POST['base_airport']
+        minimum_departure_date = request.POST['minimum_departure_date']
+        maximum_return_date = request.POST['maximum_return_date']
+        minimum_stay = request.POST['minimum_stay']
+        print(f'{base_airport=}')
+        print(f'{minimum_departure_date=}')
+        print(f'{maximum_return_date=}')
+        print(f'{minimum_stay=}')
+
+        json_res = get_route_list(base_airport, minimum_departure_date, maximum_return_date, minimum_stay)
+        res_data = json.loads(json_res)
+        # print(res_data)
+
+        result_flight = ""
+
+        for idx_airport, one_airport in enumerate(res_data, 1):
+            print(one_airport)
+            print(one_airport.keys())
+            print(one_airport['airport'])
+            airport_name = one_airport['airport']['name']
+            flight_list_outbound_count = len(one_airport['flight_list_outbound'])
+            flight_list_inbound_count = len(one_airport['flight_list_inbound'])
+            maximum_stay = one_airport['maximum_stay']
+
+            head_str = f'<strong>{airport_name}</strong>. Максимальное пребывание: {maximum_stay} часов. Рейсов {flight_list_outbound_count}/{flight_list_inbound_count}'
+
+            accordion_header = (
+                f'<div class="accordion-item">\n'    
+                    f'<h2 class="accordion-header" id="heading{idx_airport}">\n'
+                        f'<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"\n'
+                                f'data-bs-target="#collapse{idx_airport}"\n'
+                                f'aria-expanded="false" aria-controls="collapse{idx_airport}">\n'
+                            f'{head_str}\n'
+                        f'</button>\n'
+                    f'</h2>\n'
+            )
+            accordion_body_head = (
+                f'<div id="collapse{idx_airport}" class="accordion-collapse collapse" aria-labelledby="heading{idx_airport}"\n'
+                     f'data-bs-parent="#accordionExample">\n'
+            )
+
+            accordion_body_outbound_head = (
+                f'<div class="accordion-body row gx-5 accordition-body-my">\n'
+                    f'<div class="col">\n'
+                        f'<div class="row">\n'
+                            f'<div class="col-12 text-center"><strong>Вылет в {airport_name}</strong></div>\n'
+                        f'</div>\n'
+                        f'<div class="row">\n'
+                            f'<table class="table">\n'
+                                f'<thead>\n'
+                                f'<tr>\n'
+                                    f'<th scope="col">#</th>\n'
+                                    f'<th scope="col">Отправление</th>\n'
+                                    f'<th scope="col">Прибытие</th>\n'
+                                    f'<th scope="col">Рейс</th>\n'
+                                    f'<th scope="col">АК</th>\n'
+                                    f'<th scope="col">Тип ВС</th>\n'
+                                f'</tr>\n'
+                                f'</thead>\n'
+                                f'<tbody>\n'
+            )
+            accordion_body_outbound = ''
+            for idx_flight, one_flight  in enumerate(one_airport['flight_list_outbound'], 1):
+                accordion_body_outbound += (
+                                f'<tr>\n'
+                                    f'<th scope="row">{idx_flight}</th>\n'
+                                    f'<td>{pretty_datetime(one_flight["time_departure"])}</td>\n'
+                                    f'<td>{pretty_datetime(one_flight["time_arrival"])}</td>\n'
+                                    f'<td>{one_flight["flight_number"]}</td>\n'
+                                    f'<td>{one_flight["airline_name"]}</td>\n'
+                                    f'<td>{one_flight["aircraft_model"]}</td>\n'
+                                f'</tr>\n'
+                )
+            accordion_body_outbound_tail = (
+                                f'</tbody>\n'
+                            f'</table>\n'
+                        f'</div>\n'
+                    f'</div>\n'
+            )
+
+            accordion_body_inbound_head = (
+                    f'<div class="col">\n'
+                        f'<div class="row">\n'
+                            f'<div class="col-12 text-center"><strong>Возвращение из {airport_name}</strong></div>\n'
+                        f'</div>\n'
+                        f'<div class="row">\n'
+                            f'<table class="table">\n'
+                                f'<thead>\n'
+                                f'<tr>\n'
+                                    f'<th scope="col">#</th>\n'
+                                    f'<th scope="col">Отправление</th>\n'
+                                    f'<th scope="col">Прибытие</th>\n'
+                                    f'<th scope="col">Рейс</th>\n'
+                                    f'<th scope="col">АК</th>\n'
+                                    f'<th scope="col">Тип ВС</th>\n'
+                                f'</tr>\n'
+                                f'</thead>\n'
+                                f'<tbody>\n'
+            )
+
+            accordion_body_inbound = ''
+            for idx_flight, one_flight in enumerate(one_airport['flight_list_inbound'], 1):
+                accordion_body_inbound += (
+                                f'<tr>\n'
+                                    f'<th scope="row">{idx_flight}</th>\n'
+                                    f'<td>{pretty_datetime(one_flight["time_departure"])}</td>\n'
+                                    f'<td>{pretty_datetime(one_flight["time_arrival"])}</td>\n'
+                                    f'<td>{one_flight["flight_number"]}</td>\n'
+                                    f'<td>{one_flight["airline_name"]}</td>\n'
+                                    f'<td>{one_flight["aircraft_model"]}</td>\n'
+                                f'</tr>\n'
+                )
+            accordion_body_inbound_tail = (
+                                f'</tbody>\n'
+                            f'</table>\n'
+                        f'</div>\n'
+                    f'</div>\n'
+            )
+            accordion_body_tail = (
+                            f'</div>\n'
+                        f'</div>\n'
+                    f'</div>\n'
+            )
+
+            result_one_flight = (
+                accordion_header +
+                accordion_body_head +
+                accordion_body_outbound_head +
+                accordion_body_outbound +
+                accordion_body_outbound_tail +
+                accordion_body_inbound_head +
+                accordion_body_inbound +
+                accordion_body_inbound_tail +
+                accordion_body_tail
+            )
+            result_flight += result_one_flight
+
+        context = {
+            'base_airport_value': f'value={base_airport}',
+            'minimum_departure_date_value': f'value={minimum_departure_date}',
+            'maximum_return_date_value': f'value={maximum_return_date}',
+            'minimum_stay_value': f'value={minimum_stay}',
+            'result_flight': result_flight
+        }
+        print(head_str)
+
+
+
+
+    # print(context)
+    # print(request)
+
     return render(request, 'homepage/index.html', context)
---
+++ trip/trip_api.py
@@ -0,0 +1,137 @@
+import datetime as dt
+import requests
+import json
+import os
+
+BASE_URL: str = 'https://aerodatabox.p.rapidapi.com/flights/airports/icao/'
+
+
+def parse_one_flight(one_flight) -> {}:
+    new_item = {'time_departure': dt.datetime.strptime(one_flight['departure']['scheduledTimeLocal'], '%Y-%m-%d %H:%M%z'),
+                'time_arrival': dt.datetime.strptime(one_flight['arrival']['scheduledTimeLocal'], '%Y-%m-%d %H:%M%z'),
+                'flight_number': one_flight['number'],
+                'airline_name': one_flight['airline']['name']}
+
+    try:
+        new_item['aircraft_model'] = one_flight['aircraft']['model']
+    except KeyError:
+        new_item['aircraft_model'] = 'Unknown'
+    return new_item
+
+
+def date_convert_json(o):
+    if isinstance(o, (dt.date, dt.datetime)):
+        return o.isoformat()
+
+
+def get_route_list(base_airport: str, minimum_departure_date: str, maximum_return_date: str, minimum_stay: str) -> str:
+    RAPID_KEY = os.environ.get('x-rapidapi-key')
+    min_date = dt.datetime.strptime(minimum_departure_date, '%Y-%m-%dT%H:%M')
+    max_date = dt.datetime.strptime(maximum_return_date, '%Y-%m-%dT%H:%M')
+    min_stay = dt.timedelta(hours=int(minimum_stay))
+
+    cur_date = min_date  # начало периода при обращении к API
+    cur_max_date = min(cur_date + dt.timedelta(hours=12), max_date)  # конец периода при обращении к API
+    dep_list = {}  # список вылетов, попадающих в период
+    arrival_list = {}  # список прилётов, попадающих в период
+
+    # в данном цикле в dep_list и arrival_list собираем все вылеты и прилёты в базовый аэропорт за заданный интервал
+    while cur_date < max_date:  # цикл по периодам длиной 12 часов
+        cur_date_str = cur_date.strftime('%Y-%m-%dT%H:%M')
+        cur_max_date_str = cur_max_date.strftime('%Y-%m-%dT%H:%M')
+
+        url = f'{BASE_URL}{base_airport}/{cur_date_str}/{cur_max_date_str}'
+
+        headers = {
+            'x-rapidapi-key': RAPID_KEY,
+            'x-rapidapi-host': "aerodatabox.p.rapidapi.com"
+        }
+        querystring = {"withLeg": "true", "withCancelled": "false", "withCodeshared": "true",
+                       "withCargo": "false", "withPrivate": "false"}
+
+        response = requests.request("GET", url, headers=headers, params=querystring)  # дёрнули API
+        if response.status_code != 200:
+            print(response)
+            return 'Что-то пошло не так'
+
+        resp_json = response.json()
+        for one_flight in resp_json['departures']:  # цикл по всем вылетам в текущем 12-и часовом интервале
+            try:
+                icao: str = one_flight['arrival']['airport']['icao']
+            except KeyError:  # не нашли код аэропорта для рейса. Такое бывает для мелких аэропортов. Игнорируем рейс.
+                continue
+            if icao not in dep_list:  # такого аэропорта в списке ещё нет
+                dep_list[icao] = {}
+                dep_list[icao]['airport'] = one_flight['arrival']['airport']
+                dep_list[icao]['flights'] = []
+
+            new_item = parse_one_flight(one_flight)
+            # print(new_item['time_departure'].replace(tzinfo=None))
+            dep_list[icao]['flights'].append(new_item)
+
+        for one_flight in resp_json['arrivals']: # цикл по всем прилётам в текущем 12-и часовом интервале
+            try:
+                icao: str = one_flight['departure']['airport']['icao']
+            except KeyError:  # не нашли код аэропорта для рейса. Игнорируем.
+                continue
+            if icao not in arrival_list:  # такого аэропорта в списке ещё нет
+                arrival_list[icao] = {}
+                arrival_list[icao]['airport'] = one_flight['departure']['airport']
+                arrival_list[icao]['flights'] = []
+            new_item = parse_one_flight(one_flight)
+
+            # print(new_item['time_departure'].replace(tzinfo=None))
+            arrival_list[icao]['flights'].append(new_item)
+
+        cur_date = cur_max_date + dt.timedelta(minutes=1)  # начало следующего периода = конец текущего + 1 минута
+        cur_max_date = min(cur_date + dt.timedelta(hours=12, minutes=-1), max_date)
+
+    # сортируем вылеты из базового аэропорта по каждому пункту назначения в порядке возрастания времени прибытия
+    # в пункт путешествия (самый ранний вылет вначале)
+    for icao in dep_list:
+        dep_list[icao]['flights'].sort(key=lambda flight: flight['time_arrival'])
+
+    # сортируем возвращения в базовый аэропорт по каждому пункту назначения в порядке убывания времени вылета
+    # из пункта путешествия (самый поздний вылет в начале)
+    for icao in arrival_list:
+        arrival_list[icao]['flights'].sort(key=lambda flight: flight['time_departure'], reverse=True)
+
+    # финальный рывок - отбираем аэропорты и рейсы, которые подпадают под ограничения.
+    ret_data = []  # итоговый список. один элемент списка - одно направление (аэропорт) и рейсы в/из него
+    for icao in dep_list:
+        minimum_time_arrival = dep_list[icao]['flights'][0]['time_arrival']
+        try:
+            maximum_time_departure = arrival_list[icao]['flights'][0]['time_departure']
+        except KeyError:  # вылет есть, возврата нет. это направление нам не подходит.
+            continue
+        maximum_stay = maximum_time_departure - minimum_time_arrival
+        # print(f'{icao=} {minimum_time_arrival=:%Y-%m-%d %H:%M:%S}   {maximum_time_departure=:%Y-%m-%d %H:%M:%S}  {maximum_stay=} {min_stay=}')
+        if maximum_stay < min_stay:
+            # print(f'Хрен вам, а не {icao}')
+            continue
+        one_airport = {
+                       'airport': dep_list[icao]['airport'],
+                       'maximum_stay': round(maximum_stay.total_seconds() / 3600),
+                       'flight_list_outbound': []
+        }
+
+        for one_flight in dep_list[icao]['flights']:
+            if one_flight['time_arrival'] + min_stay < maximum_time_departure:
+                one_airport['flight_list_outbound'].append(one_flight)
+
+        one_airport['flight_list_inbound'] = []
+        for one_flight in arrival_list[icao]['flights']:
+            if one_flight['time_departure'] - min_stay > minimum_time_arrival:
+                one_airport['flight_list_inbound'].append(one_flight)
+        ret_data.append(one_airport)
+
+    ret_data.sort(key=lambda airport: airport['maximum_stay'], reverse=True)
+
+    return json.dumps(ret_data, default=date_convert_json)
+
+
+def pretty_datetime(s: str) -> str:
+    date = dt.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S%z')
+    return date.replace(tzinfo=None).strftime('%Y-%m-%d %H:%M')
+
+
